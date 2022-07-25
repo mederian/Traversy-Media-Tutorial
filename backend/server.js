f68e18d19180c89d7f6c463ea1f7e14e287da840
Ole Marius Snekkermo <ole.snekkermo@gmail.com>
2022-07-25 23:45:56 +0200

    All done

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
+const { json } = require('express')
 const express = require('express')
-const dotenv = require('dotenv').config
+const dotenv = require('dotenv').config()
 const port = process.env.port
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
 
 app.listen(port, () => console.log(`server started on port  ${port}`))
